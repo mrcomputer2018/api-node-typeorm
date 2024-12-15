7ba42a71718525d8b54671e31381b88d1964d569
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-12-15 10:21:53 -0300

    feat: refactor JWT authentication by moving Auth class to a new file and implementing middleware for token validation

---
+++ src/app/middlewares/AuthMiddleware.ts
@@ -0,0 +1,19 @@
+import ErrorExtension from "../utils/ErrorExtension";
+import { Request, Response, NextFunction } from "express";
+import Auth from "../utils/auth";
+
+
+export const AuthenticateMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization as string;
+
+    if (!token) {
+        throw new ErrorExtension(401, 'Unauthorized');
+    }
+
+    const auth = new Auth();
+
+    //verificando se token esta valido
+    auth.AuthenticatedToken(token);
+
+    next();
+}
